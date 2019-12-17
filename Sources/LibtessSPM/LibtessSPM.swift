e44499eaa9673ec83c3413d35ac602c02d53e01c
Vasiliy <Freeneos@icloud.com>
2019-12-17 15:43:43 +0300

    Initial commit

--- Sources/LibtessSPM/LibtessSPM.swift
+++ Sources/LibtessSPM/LibtessSPM.swift
@@ -1,3 +1,106 @@
-struct LibtessSPM {
-    var text = "Hello, World!"
+
+import CLibtess2
+
+public enum TessOption: Int {
+    case constrainedDelanayTriangulation
+    case reverseContours
+}
+
+public enum WindingRule: Int {
+    case odd
+    case nonZero
+    case positive
+    case negative
+    case absGeqTwo
+}
+
+public enum ElementType: Int {
+    case polygons
+    case connectedPolygons
+    case boundaryContours
+}
+
+public enum VertexSize: Int {
+    case size2 = 2
+    case size3 = 3
+}
+
+public enum TessError: Error {
+    /// Error when a tessTesselate() call fails.
+    case tesselationFailed
+    /// Error thrown by TessC.tesselate static method when TessC initialization
+    /// failed.
+    case tessCInitFailed
+}
+
+public class LibtessSPM {
+    
+    var tess: UnsafeMutablePointer<TESStesselator>
+    
+    public init?() {
+        guard let tess = tessNewTess(nil) else {
+            print("Failed to initialize TESStesselator")
+            return nil
+        }
+        self.tess = tess
+    }
+    
+    deinit {
+        tessDeleteTess(tess)
+    }
+    
+    // setOption() - Toggles optional tessellation parameters
+    // Parameters:
+    //  option - one of TessOption
+    //  value - 1 if enabled, 0 if disabled.
+    public func setOption(optin: TessOption, value: Int) {
+        tessSetOption(tess, Int32(optin.rawValue), Int32(value))
+    }
+    
+    open func addContour(size: Int, vertices: UnsafeRawPointer, stride: Int, count: Int) { //MemoryLayout<CVector3>.size
+        tessAddContour(tess, CInt(size), vertices, CInt(stride), CInt(count))
+    }
+    
+    open func tesselate(windingRule: WindingRule, elementType: ElementType, polySize: Int, vertexSize: VertexSize)-> (vertices: [TESSreal], indices: [Int])? {
+        
+        if tessTesselate(tess, CInt(windingRule.rawValue), CInt(elementType.rawValue), CInt(polySize), CInt(vertexSize.rawValue), nil) == 0 {
+            print("Error")
+            return nil
+        }
+        
+        tessGetElements(tess)
+        guard let vertices = tess.pointee.vertices, let elements = tess.pointee.elements else {
+            print("Error")
+            return nil
+        }
+        let vertexCount = Int(tess.pointee.vertexCount)
+        let elementCount = Int(tess.pointee.elementCount)
+        let stride = vertexSize.rawValue
+        
+        var output: [TESSreal] = Array(repeating: 0, count: vertexCount * stride)
+        output.withUnsafeMutableBufferPointer { (body) -> Void in
+            body.baseAddress?.assign(from: vertices, count: vertexCount * stride)
+        }
+        
+        var indicesOut: [Int] = []
+        
+        for i in 0..<elementCount {
+            let p = elements.advanced(by: i * polySize)
+            for j in 0..<polySize where p[j] != ~TESSindex() {
+                indicesOut.append(Int(p[j]))
+            }
+        }
+        
+//        verticesRaw = output
+//        vertexCount = nverts
+//        elementCount = nelems
+//
+//        elements = indicesOut
+        
+        return (output, indicesOut)
+        
+    }
+    
+    
+    
 }
--- Tests/LibtessSPMTests/LibtessSPMTests.swift
+++ Tests/LibtessSPMTests/LibtessSPMTests.swift
@@ -2,14 +2,82 @@ import XCTest
 @testable import LibtessSPM
 
 final class LibtessSPMTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(LibtessSPM().text, "Hello, World!")
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(LibtessSPM().text, "Hello, World!")
+//    }
+//
+//    static var allTests = [
+//        ("testExample", testExample),
+//    ]
+    
+    func testLibtessSPM() {
+        let tess = LibtessSPM()
+        
+        XCTAssert(tess != nil, "LibtessSPM = nil")
     }
-
+    
+//    switch(option) {
+//    case TESS_CONSTRAINED_DELAUNAY_TRIANGULATION:
+//        tess->processCDT = value > 0 ? 1 : 0;
+//    case TESS_REVERSE_CONTOURS:
+//        tess->reverseContours = value > 0 ? 1 : 0;
+//    }
+    
+    func testSetOption() {
+        let tessOpt = LibtessSPM()
+        
+        guard let tess = tessOpt else {
+            XCTAssertNil(tessOpt, "LibtessSPM = nil")
+            return
+        }
+        
+        let defaultProcessCDT = tess.tess.pointee.processCDT
+        let defaultReverseContours = tess.tess.pointee.reverseContours
+        
+        tess.setOption(optin: .constrainedDelanayTriangulation, value: 1)
+        XCTAssert(defaultProcessCDT < tess.tess.pointee.processCDT, "set optional processCDT \(tess.tess.pointee.processCDT)")
+        
+        tess.setOption(optin: .reverseContours, value: 1)
+        XCTAssert(defaultReverseContours < tess.tess.pointee.reverseContours, "set optional processCDT \(tess.tess.pointee.reverseContours)")
+        
+    }
+    
+    func testAddContourTesselate() {
+        let tessOpt = LibtessSPM()
+        
+        guard let tess = tessOpt else {
+            XCTAssertNil(tessOpt, "LibtessSPM = nil")
+            return
+        }
+        
+        var vertices: [Vertex] = []
+        vertices.append(Vertex(x: 0.0, y: 0.0))
+        vertices.append(Vertex(x: 0.0, y: 10.0))
+        vertices.append(Vertex(x: 10.0, y: 10.0))
+        vertices.append(Vertex(x: 0.0, y: 10.0))
+        vertices.append(Vertex(x: 0.0, y: 0.0))
+        
+        tess.addContour(size: 2, vertices: vertices, stride: MemoryLayout<Vertex>.size, count: vertices.count)
+        
+        let polygonIndexCount = 3; // triangles only
+        let result = tess.tesselate(windingRule: .odd, elementType: .polygons, polySize: polygonIndexCount, vertexSize: .size2)
+        print("result = ", result?.vertices.count, result?.indices.count)
+        XCTAssert(result != nil, "Result tesselate = nil")
+        
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testLibtessSPM", testLibtessSPM),
+        ("testSetOption", testSetOption),
+        ("testAddContourTesselate", testAddContourTesselate)
     ]
+    
+}
+
+struct Vertex {
+    let x: Float
+    let y: Float
 }
